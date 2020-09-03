f1d502f0e3bd9b52cfdc3ddfb19cf69a436eb8d4
Younes Rahmouni <youneskendji@gmail.com>
2020-09-03 16:28:27 +0100

    Update config vars.

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const port = 3001;
-const cors = require('cors');
-const path = require('path');
+const cors = require("cors");
+const path = require("path");
 
 // Initialize ENV
 dotenv.config();
@@ -13,26 +13,28 @@ dotenv.config();
 app.use(cors());
 
 // Expose videos & covers
-app.use('/static', express.static(path.join(__dirname, 'uploads')));
+app.use("/static", express.static(path.join(__dirname, "uploads")));
+
+const connectionString = `mongodb+srv://younesrh:${process.env.DB_PASSWORD}@cluster0.f0u2a.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 // Connect to database
 const db = mongoose
-  .connect(process.env.DB_CONNECT_URL, {
+  .connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('db connected');
+    console.log("db connected");
   });
 
 // import routes.
-const auth = require('./routes/auth');
-const data = require('./routes/data');
+const auth = require("./routes/auth");
+const data = require("./routes/data");
 
 // Middlewares
 app.use(express.json());
-app.use('/api/auth', auth);
-app.use('/api/', data);
+app.use("/api/auth", auth);
+app.use("/api/", data);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
